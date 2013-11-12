9b9db2ecb4e80b1aa38374b538422903e3352841
Davis Vilums <davisvilums@gmail.com>
2013-11-12 13:05:36 +0000

    Codeigniter tutorial 6/11

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -3,12 +3,11 @@
 class Site extends CI_Controller {
 
 	public function index(){
-		echo "Hi internet! <br/>";
 		$this->home();
 	}
 
 	public function home(){
-		$data['title'] = "Welcome!";
+		$data['title'] = "Home!";
 		$data['var1'] = "2";
 		$data['var2'] = "8";
 
@@ -17,5 +16,10 @@ class Site extends CI_Controller {
 		$data['addTotal'] = $this->math->add($data['var1'],$data['var2']);
 		$data['subTotal'] = $this->math->sub($data['var1'],$data['var2']);
 		$this->load->view("view_home", $data);
+	}
+
+	function about() {
+		$data['title'] = "About!";
+		$this->load->view("view_about");
 	}
 }
